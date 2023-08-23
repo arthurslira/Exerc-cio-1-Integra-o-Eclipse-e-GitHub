5e31b88e8c21ccb39b9ca2cd60252a464dd54656
arthurslira <130676722+arthurslira@users.noreply.github.com>
2023-08-23 17:53:47 -0300

    Add files via upload

---
+++ arquivo.java
@@ -0,0 +1,19 @@
+import java.util.*;
+ class arquivo{
+	 
+	
+	 
+	 public static void main (String args[]) {
+	 int num1,num2,soma;
+     Scanner sc = new Scanner(System.in);
+	 
+     System.out.println("Digite um número: ");
+     num1=sc.nextInt();
+     System.out.println("Digite outro número: ");
+     num2=sc.nextInt();
+     
+     soma=num1 + num2;
+     
+     System.out.println("Soma="+soma);
+	 }
+}
